a5706e7193e664ea379e0810bc3d07b7f6aa9870
Samori Gorse <samorigorse@gmail.com>
2012-03-16 17:15:21 +0100

    Added basic style setup

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var cp = require('child_process');
 
 var pushover = require('pushover');
 var express = require('express');
@@ -7,66 +6,33 @@ var express = require('express');
 var Repo = require('./lib/repo');
 var util = require('./lib/util');
 
-var spawn = cp.spawn;
-
 const GITROOT = '.pushstack';
 const GITURL = '/git/*';
-const GITCLONESROOT = '.clones';
-
-//Local clone management
-function createLocalClone(dir) {
-    var clone = spawn('git', [ 'clone', path.join(GITROOT, dir), util.getLocalClonePath(dir) ]);
-    clone.on('exit', function(code) {
-        console.log('clone exited with code: ' + code);
-    });
-}
-
-//Local clone management
-function updateLocalClone(dir) {
-    var target = util.getLocalClonePath(dir);
-    var pull = spawn('git', ['pull'], {'cwd': target});
-    pull.on('exit', function(code) {
-        console.log('pull exited with code: ' + code);
-    });
-}
-
-//Local clone management
-function checkoutLocalClone(dir, ref, cb) {
-    var target = util.getLocalClonePath(dir);
-    var checkout = spawn('git', [ 'checkout', ref ], {'cwd': target});
-    checkout.on('exit', function(code) {
-        console.log('checkout exited with code: ' + code);
-        if(!code) {
-            cb(null);
-        } else {
-            cb('OUPS');
-        }
-    });
-}
 
 
 var repos = pushover(path.join(__dirname, GITROOT));
-repos.on('create', function create(dir) {
-    util.createLocalClone(dir);
-});
-repos.on('push', function push(dir) {
-    util.updateLocalClone(dir);
-});
+repos.on('create', util.createLocalClone);
+repos.on('push', util.updateLocalClone);
 
 var app = express.createServer();
 app.use(express.bodyParser());
 app.use(express.favicon());
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view options', {layout: false});
 
+
 function proxyGitRequest(req, res) {
+    //TODO: Should reverse the proxied URL
     req.url = util.truncateHead(req.url);
     repos.handle(req, res);
 }
 
 function checkoutRef(req, res) {
-    var repo = new Repo(req.body.repo);
+    var name = req.body.repo,
+        repo = new Repo(name);
+
     repo.checkout(req.body.ref, function() {
-        res.redirect('/' + req.body.repo + '/');
+        res.redirect('/' + name + '/');
     });
 }
 
@@ -74,8 +40,6 @@ function list(req, res) {
     var name = req.params.name || req.params[0],
         entry = req.params[1] || '',
         repo = new Repo(name);
-
-    console.log(entry, name);
 
     repo.list(entry, function(items, branches, tags) {
         if(!items) {
