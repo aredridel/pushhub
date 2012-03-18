11c0bf2ac3dd494b2fc54b55a8c4067d2b089c79
Samori Gorse <samorigorse@gmail.com>
2012-03-18 19:40:44 +0100

    Added breadcrumb

--- app.js
+++ app.js
@@ -22,6 +22,18 @@ function makePreview(url, data) {
     }
 }
 
+function parents(root, p) {
+    var parts = p.split('/');
+    parts = parts.map(function(part, index) {
+        return {
+            'label': part,
+            'url': '/' + root + '/' + 'tree' + '/'  + parts.slice(0, index + 1).join('/')
+        };
+    });
+    parts[parts.length - 1].isLast = true;
+    return parts;
+}
+
 function handleGitRequest(req, res) {
     var parts = req.url.split('/');
     req.url = '/' + parts.slice(2).join('/');
@@ -51,6 +63,7 @@ function tree(req, res) {
     repo.tree(entry, function(items, branches, tags) {
         if(!items) { res.render('404.jade'); }
         res.local('repo', name);
+        res.local('parents', parents(name, entry));
         res.local('items', items);
         res.local('branches', branches);
         res.local('tags', tags);
@@ -70,6 +83,7 @@ function blob(req, res) {
     repo.blob(entry, function(err, data) {
         if(err) { throw err; }
         res.local('repo', name);
+        res.local('parents', parents(name, entry));
         res.local('preview', makePreview(req.url, data));
         res.render('display.jade', res.locals());
     });
