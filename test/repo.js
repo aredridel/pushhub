6e686807bd0ecc10e70a39914382419487fe9ac8
Samori Gorse <samorigorse@gmail.com>
2012-04-12 08:36:40 +0200

    Repo#stat

--- test/repo.js
+++ test/repo.js
@@ -2,7 +2,8 @@ var assert = require('assert');
 
 var Repo = require('../lib/repo');
 
-var r = new Repo(__dirname + '/sandbox/fail');
+var repo = 'fail';
+var r = new Repo(__dirname + '/sandbox/' + repo);
 
 describe(__filename, function() {
     it('should distinguish bare repo from full repository', function() {
@@ -15,5 +16,31 @@ describe(__filename, function() {
             assert.ok(count > 0);
             done();
         });
+    });
+
+    it('should stat the given path', function(done) {
+       r.stats('master', '.', function(err, entry) {
+           assert.ifError(err);
+           assert.equal(entry.url, '/' + repo + '/master/.');
+           assert.ok(entry.commits.size() > 0);
+           done();
+       });
+    });
+
+    it('should stat 2 commits', function(done) {
+       r.stats('master', '.', 2, function(err, entry) {
+           assert.ifError(err);
+           assert.equal(entry.commits.size(), 2);
+           done();
+       });
+    });
+
+    it('should stat 5 commits and skip 2', function(done) {
+       r.stats('master', '.', 5, 2, function(err, entry) {
+           //TODO: Fix this test
+           assert.ifError(err);
+           assert.equal(entry.commits.size(), 5);
+           done();
+       });
     });
 });
