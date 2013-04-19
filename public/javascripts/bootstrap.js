93e34f54aaa66c9782a36882b0c098aacb95630b
Samori Gorse <samorigorse@gmail.com>
2013-04-19 04:28:05 +0200

    Added preview client side logic

--- public/javascripts/bootstrap.js
+++ public/javascripts/bootstrap.js
@@ -1,6 +1,7 @@
 $(function() {
     var $body = $(document.body);
     var $containers = $body.find('.container');
+    var readme = $('.tree-list .name').filter(function(index, el) { return el.innerText.match(/^readme/i) });
 
     $containers.click(function(e) {
         $(this).toggleClass('open');
@@ -21,11 +22,21 @@ $(function() {
             var $el = $(el);
             var val = $el.val();
             var repo = $el.data('repo');
-            $.post('/' + $el.data('repo') + '/description', { description: val }, function() {
+            $.post('/' + repo + '/description', { description: val }, function() {
                 $el.replaceWith($('<p data-repo="' + repo + ' class="description">' + val + '</p>'));
             })
         });
     });
 
-    Sunlight.highlightAll();
+    if(readme.length > 0 && $body.data('view') === 'tree') {
+      $.get('/' + [$body.data('repo'), 'preview', $body.data('ref'), readme.get(0).innerText].join('/'), function(json) {
+        var preview = $('<div class="preview padding15"></div>').html(json.data);
+        var title = $('<h2/>').text(json.path);
+        $('.tree-list-wrapper').after(title, preview);
+      });
+    }
+
+    if($body.data('view') === 'blob') {
+      Sunlight.highlightAll();
+    }
 });
